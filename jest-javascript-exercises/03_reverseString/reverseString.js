9754c661f8fcd290baea6844c0b909339fdab4d5
ima4y <ashleygrobicheau@gmail.com>
2022-03-18 07:53:43 -0600

    03_reverseString all tests passed

--- jest-javascript-exercises/03_reverseString/reverseString.js
+++ jest-javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Chained: return string.split("").reverse().join("");
+    
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray; 
 };
 
 // Do not edit below this line
