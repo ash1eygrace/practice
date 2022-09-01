002d7f3cbeda36c06c4a9c794d601046167922ec
ash1eygrace <ashleygrobicheau@gmail.com>
2022-09-01 15:35:57 -0600

    Move date and time apps to one html and js file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## JS Practice
 This repo is used to practice and learn JavaScript deeply.
 
-I'm moostly taking insporation from and working through the exercises here: https://www.w3resource.com/javascript-exercises/
+I'm mostly taking insporation from and working through the exercises here: https://www.w3resource.com/javascript-exercises/
--- components/DatesandTime.js
+++ components/DatesandTime.js
@@ -1,6 +1,29 @@
-/* display the current day and time in the #content div of the index.html page example:
-Today is: Wednesday 31, 2022
-Current time is: 7:20AM */
+/*
+1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+Sample Output : Today is : Tuesday.
+Current time is : 10 PM : 30 : 38
+*/
+document.getElementById("content").innerHTML = `<p>Today is: <span id="day"></span>.</p><p>Current time is: <span id="time"></span>.</p>`;
+
+const day = document.getElementById('day');
+const time = document.getElementById('time');
+day.innerText = new Date().toLocaleString('en-US', { weekday: 'long' });
+time.innerText = new Date().toLocaleTimeString();
+
+/* This mini app asks for a future date and displays how many days between now and that future date */
+var form = document.querySelector('content');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var date = document.querySelector('input[type="date"]').value;
+    var today = new Date();
+    var future = new Date(date);
+    var days = Math.ceil((future - today) / (1000 * 60 * 60 * 24));
+    var p = document.createElement('p');
+    p.innerHTML = 'There are ' + days + ' days until ' + date;
+    document.body.appendChild(p);
+});
+
+/* Displays the current time on the screen and cycles through it */
 const displayTime = () => {
     const today = new Date();
     const day = today.getDay();
@@ -10,3 +33,4 @@ const displayTime = () => {
     document.getElementById("content").innerHTML = `Today is: ${dayNames[day]}, ${today.getDate()}, ${today.getFullYear()}<br>Current time is: ${time} ${amPm}`;
 }
 setInterval(displayTime, 1000);
+
