3ce07ad80a3810056025d3d55e833add8771a767
ash1eygrace <ashleygrobicheau@gmail.com>
2023-01-23 09:30:19 -0700

    Convert React.createElement to jsx in app.js

--- react-basics/app.js
+++ react-basics/app.js
@@ -1,14 +1,20 @@
-const title = React.createElement(
-    'h1', 
-    { id: 'main-title', title: 'This is a title.' }, 
-    'My First React Element!'
-);
+/*
+    const title = React.createElement(
+        'h1', 
+        { id: 'main-title', title: 'This is a title.' }, 
+        'My First React Element!'
+    );
 
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned to create a React node and render it into the DOM.'
-);
+    const desc = React.createElement(
+        'p',
+        null,
+        'I just learned to create a React node and render it into the DOM.'
+    );
+*/
+
+// Convert the above into JSX 
+const title = <h1>My First React Element!</h1>;
+const desc = <p>I just learned to create a React node and render it into the DOM.</p>;
 
 const header = React.createElement(
     'header',
