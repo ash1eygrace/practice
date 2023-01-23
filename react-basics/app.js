1d465f34deceaa6ed68d20a8753b6889bd696750
ash1eygrace <ashleygrobicheau@gmail.com>
2023-01-23 09:57:11 -0700

    Convert the JSX to use JavaScript Expressions

--- react-basics/app.js
+++ react-basics/app.js
@@ -10,9 +10,9 @@
         null,
         'I just learned to create a React node and render it into the DOM.'
     );
-*/
 
-// Convert the above into JSX 
+// Convert the above into JSX: 
+
 const title = <h1>My First React Element!</h1>;
 const desc = <p>I just learned to create a React node and render it into the DOM.</p>;
 
@@ -22,6 +22,23 @@ const header = React.createElement(
     title,
     desc
 );
+*/
+
+// Embed JavaScript Expressions in JSX 
+
+const title = 'My First React Element!';
+const desc = 'I just learned to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
+const myName = 'Ashley';
+
+const header = (
+    <header>
+        <h1 id={myTitleID}>{ title }</h1>
+        <h2> { myName }'s learning stuff. </h2>
+        <p>{ desc }</p>
+    </header>
+);
+
 
 ReactDOM.render(
     header,
