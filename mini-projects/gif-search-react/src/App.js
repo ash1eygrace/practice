79e269d10ba5ee62bc8fb23051b0c8074d0ca7b4
ash1eygrace <ashleygrobicheau@gmail.com>
2023-02-26 08:10:44 -0700

    Add Giphy search app to mini-projects directory

--- mini-projects/gif-search-react/src/App.js
+++ mini-projects/gif-search-react/src/App.js
@@ -1,18 +1,47 @@
-import React from "react";
-import SearchForm from './Components/SearchForm';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("hey");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let activeFetch = true;
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=2dnOc3I29EDM935Yi5M8FhOLoK7fC0vK&limit=24&rating=g`)
+      .then(response => {
+        if (activeFetch) {
+          setGifs(response.data.data);
+          setLoading(false);
+        }
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data from Giphy.", error);
+      })
+    return () => { activeFetch = false; }
+  }, [query]);
+
+  const handleQueryChange = searchText => {
+    setQuery(searchText);
+  }
+
   return (
     <div>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+          <SearchForm changeQuery={handleQueryChange} />
         </div>
       </div>
       <div className="main-content">
-        <GifList />
+        {
+          (loading)
+          ? <p>Loading...</p>
+          : <GifList data={gifs} />
+        }
       </div>
     </div>
   );
--- mini-projects/gif-search-react/src/Components/NoGifs.js
+++ mini-projects/gif-search-react/src/Components/NoGifs.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const NoGifs = props => (
-  <li className='no-gifs'>
+  <li className="no-gifs">
     <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
     <h3>Sorry, no GIFs match your search.</h3>
   </li>
