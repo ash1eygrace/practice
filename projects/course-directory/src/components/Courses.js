e280eab19d3d245e22305f2af8177e78f1dc69bd
ash1eygrace <ashleygrobicheau@gmail.com>
2023-03-06 07:52:29 -0700

    add nested routes to app.js for courses page html, css, and js links

--- projects/course-directory/src/components/Courses.js
+++ projects/course-directory/src/components/Courses.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='#'>HTML</a></li>
-        <li><a href='#'>CSS</a></li>
-        <li><a href='#'>JavaScript</a></li>
+        <li><NavLink to='HTML'>HTML</NavLink></li>
+        <li><NavLink to='CSS'>CSS</NavLink></li>
+        <li><NavLink to='JavaScript'>JavaScript</NavLink></li>
       </ul>
     </div>
+    <Outlet />
   </div>
 );
 
