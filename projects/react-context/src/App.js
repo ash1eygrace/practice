b20b19f62602270bf029e527bc57aa206aa71b76
ash1eygrace <ashleygrobicheau@gmail.com>
2023-04-13 06:53:34 -0600

    refactor theme provider to use context

--- projects/react-context/src/App.js
+++ projects/react-context/src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 // App Components
@@ -10,58 +9,17 @@ import Settings from "./components/Settings";
 import NotFound from "./components/NotFound";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [accentColor, setAccentColor] = useState('#63537d');
-  const [fontPercentage, setFontPercentage] = useState(100);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-    document.body.style.fontSize = `${fontPercentage}%`;
-  }, [isDarkMode, fontPercentage]);
-
-  const signInUser = (username, password) => {
-    const newUser = {
-      username,
-      password
-    };
-    setUser(newUser);
-  }
-
-  const signOutUser = () => {
-    setUser(null);
-  }
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(currentMode => !currentMode);
-  }
-
   return (
-      <div>
-        <Header accentColor={accentColor} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="signin" element={
-            <UserSignIn accentColor={accentColor} />
-          } />
-          <Route path="signout" element={<UserSignOut />} />
-          <Route path="settings" element={
-            <Settings
-              isDarkMode={isDarkMode}
-              toggleDarkMode={toggleDarkMode}
-              accentColor={accentColor}
-              updateAccentColor={setAccentColor}
-              fontPercentage={fontPercentage}
-              updateFontPercentage={setFontPercentage} />
-          } />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signin" element={<UserSignIn />} />
+        <Route path="signout" element={<UserSignOut />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
--- projects/react-context/src/components/Settings.js
+++ projects/react-context/src/components/Settings.js
@@ -7,7 +7,7 @@ import DarkMode from "./themes/DarkMode";
 import AccentColor from "./themes/AccentColor";
 import FontSize from "./themes/FontSize";
 
-function Settings(props) {
+function Settings() {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -22,16 +22,9 @@ function Settings(props) {
     <div className="bounds">
       <div className="grid-100">
         <h1>Preferences</h1>
-        <DarkMode
-          isDarkMode={props.isDarkMode}
-          toggleDarkMode={props.toggleDarkMode} />
-        <AccentColor
-          accentColor={props.accentColor}
-          updateAccentColor={props.updateAccentColor} />
-        <FontSize
-          accentColor={props.accentColor}
-          fontPercentage={props.fontPercentage}
-          updateFontPercentage={props.updateFontPercentage} />
+        <DarkMode />
+        <AccentColor />
+        <FontSize/>
       </div>
     </div>
   );
