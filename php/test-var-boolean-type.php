591f6b2bfa2dc3fb094d9cd87d37c0949396e3c6
ash1eygrace <ashleygrobicheau@gmail.com>
2023-06-13 15:58:00 -0600

    Added test-var-boolean-type.php to new php directory

---
+++ php/test-var-boolean-type.php
@@ -0,0 +1,15 @@
+<?php 
+
+// check the the boolean value of each variable
+
+$name = 'Treehouse';
+$num = -2;
+$bool = true;
+$time = 0; 
+
+echo "The boolean value of \$name is: ".(boolval($name) ? 'TRUE' : 'FALSE')."<br>";
+echo "The boolean value of \$num is: ".(boolval($num) ? 'TRUE' : 'FALSE')."<br>";
+echo "The boolean value of \$bool is: ".(boolval($bool) ? 'TRUE' : 'FALSE')."<br>";
+echo "The boolean value of \$time is: ".(boolval($time) ? 'TRUE' : 'FALSE')."<br>";
+
+?>
