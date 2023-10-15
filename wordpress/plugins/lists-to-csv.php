17ab19bf418aed97cc3dfcd88d51a1c956a3c859
ash1eygrace <ashleygrobicheau@gmail.com>
2023-10-15 16:31:17 -0600

    add date selection and URl of post for list items to lists-to-csv.php

--- wordpress/plugins/lists-to-csv.php
+++ wordpress/plugins/lists-to-csv.php
@@ -1,21 +1,31 @@
 <?php
 /*
 Plugin Name: Lists to CSV
-Description: Extracts all ordered and unordered lists from posts and exports to CSV.
-Version: 1.0
+Description: Extracts all ordered and unordered lists from posts within a specified date range and exports them to CSV, alongside their post URLs.
+Version: 1.1
 Author: @ash1eygrace
 */
 
 /**
- * Fetch lists (both ordered and unordered) from all posts.
+ * Fetch lists (both ordered and unordered) from all posts within a given date range 
+ * and return them along with the post's permalink.
  *
- * @return array An array containing the content of list items.
+ * @param string $startDate The start date in 'YYYY-MM-DD' format.
+ * @param string $endDate The end date in 'YYYY-MM-DD' format.
+ * @return array An array of associative arrays with 'content' for the list content and 'permalink' for the post's URL.
  */
-function fetch_lists_from_posts() {
+function fetch_lists_from_posts($startDate = '', $endDate = '') {
     $args = array(
-        'posts_per_page' => -1,  // Fetch all posts.
+        'posts_per_page' => -1,
         'post_type' => 'post',
         'post_status' => 'publish',
+        'date_query' => array(
+            array(
+                'after'     => $startDate,
+                'before'    => $endDate,
+                'inclusive' => true,
+            ),
+        ),
     );
 
     $posts = get_posts($args);
@@ -24,7 +34,10 @@ function fetch_lists_from_posts() {
     foreach ($posts as $post) {
         if (preg_match_all('/<li>(.*?)<\/li>/', $post->post_content, $matches)) {
             foreach ($matches[1] as $match) {
-                $lists_data[] = strip_tags($match);
+                $lists_data[] = array(
+                    'content' => strip_tags($match),
+                    'permalink' => get_permalink($post->ID)
+                );
             }
         }
     }
@@ -33,20 +46,27 @@ function fetch_lists_from_posts() {
 }
 
 /**
- * Trigger CSV download containing the lists if the download_lists_csv query parameter is set.
+ * Trigger CSV download containing the lists and their post permalinks if the download_lists_csv query parameter is set.
  *
  * @return void
  */
 function download_lists_as_csv() {
     if (isset($_GET['download_lists_csv'])) {
+        $startDate = isset($_GET['start_date']) ? $_GET['start_date'] : '';
+        $endDate = isset($_GET['end_date']) ? $_GET['end_date'] : '';
+
         header('Content-Type: text/csv');
         header('Content-Disposition: attachment; filename="lists.csv"');
 
-        $data = fetch_lists_from_posts();
+        $data = fetch_lists_from_posts($startDate, $endDate);
 
         $output = fopen('php://output', 'w');
+        
+        // Adding header row to CSV
+        fputcsv($output, array("List Content", "Post URL"));
+
         foreach ($data as $line) {
-            fputcsv($output, array($line));
+            fputcsv($output, array($line['content'], $line['permalink']));
         }
 
         fclose($output);
@@ -66,12 +86,22 @@ function lists_to_csv_menu() {
 }
 
 /**
- * Display the link to trigger the CSV download on the custom admin page.
+ * Display the form to choose a date range and the link to trigger the CSV download on the custom admin page.
  *
  * @return void
  */
 function download_lists_as_csv_page() {
-    echo '<a href="' . admin_url('?download_lists_csv=true') . '">Download Lists as CSV</a>';
+    // Check if dates are set
+    $startDate = isset($_POST['start_date']) ? $_POST['start_date'] : '';
+    $endDate = isset($_POST['end_date']) ? $_POST['end_date'] : '';
+
+    echo '<form method="post" action="">';
+    echo 'Start Date: <input type="date" name="start_date" value="' . esc_attr($startDate) . '">';
+    echo 'End Date: <input type="date" name="end_date" value="' . esc_attr($endDate) . '">';
+    echo '<input type="submit" value="Set Date Range">';
+    echo '</form>';
+
+    echo '<a href="' . admin_url('?download_lists_csv=true&start_date=' . $startDate . '&end_date=' . $endDate) . '">Download Lists as CSV</a>';
 }
 
 add_action('admin_menu', 'lists_to_csv_menu');
